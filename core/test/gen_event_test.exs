fa7a4f9a948ceb0315a9f01a4ba52bbd4780dca6
yurifrl <yurifrl@hotmail.com>
2016-09-03 21:16:22 -0300

    test

---
+++ core/test/gen_event_test.exs
@@ -0,0 +1,13 @@
+defmodule GenEventTest do
+  use ExUnit.Case
+  doctest Core
+
+  alias Core.Messenger.EventServer
+  alias Core.Messenger.Handlers
+
+  test "test gen event" do
+    { :ok, _} = EventServer.start_link []
+    :ok = EventServer.add_handler Handlers.GreetingHandler
+    :ok = Handlers.notify {:message, 'GreetingMessage'}
+  end
+end
