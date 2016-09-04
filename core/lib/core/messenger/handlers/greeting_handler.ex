aaba4fab1d25ab3fd82ae2644dcf6afa05e4395c
gabrielcorado <gabrielcorado@mail.com>
2016-09-04 05:07:03 -0300

    Testing the Sender

--- core/lib/core/messenger/handlers/greeting_handler.ex
+++ core/lib/core/messenger/handlers/greeting_handler.ex
@@ -2,14 +2,20 @@ defmodule Core.Messenger.Handlers.GreetingHandler do
   #
   use GenEvent
 
+  # Alias
+  alias Core.Messenger.Sender
+
   @moduledoc """
   """
 
   @doc """
   """
-  def handle_event({:message, 'GreetingMessage'}, messages) do
+  def handle_event({:message, 'GreetingMessage', recipient}, messages) do
     #
     IO.inspect messages
+
+    # Send a message
+    Sender.send recipient, "Bom dia!"
 
     # OK!
     { :ok, messages }
