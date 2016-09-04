d81e28483c163e8efc048160258cbd675808f90d
gabrielcorado <gabrielcorado@mail.com>
2016-09-03 23:41:27 -0300

    Starts `Messenger.EventServer` in the application with Supervisor

--- core/lib/core.ex
+++ core/lib/core.ex
@@ -10,11 +10,12 @@ defmodule Core do
     children = [
       # Starts a worker by calling: Core.Worker.start_link(arg1, arg2, arg3)
       worker(Core.Database, [[host: "rethinkdb", port: 28015]]),
-      Plug.Adapters.Cowboy.child_spec(:http, Core.API, [], [port: 8000])
+      Plug.Adapters.Cowboy.child_spec(:http, Core.API, [], [port: 8000]),
+      worker(Core.Messenger.EventServer, [[]])
     ]
 
-    # # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # # for other strategies and supported options
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Core.Supervisor]
     Supervisor.start_link(children, opts)
   end
