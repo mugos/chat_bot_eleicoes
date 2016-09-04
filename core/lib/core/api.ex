75e926b2f65d9e1c68dfb4c838c7439d5c84f908
gabrielcorado <gabrielcorado@mail.com>
2016-09-03 22:49:59 -0300

    Make challange and message endpoints work

--- core/lib/core/api.ex
+++ core/lib/core/api.ex
@@ -15,6 +15,12 @@ defmodule Core.API do
   plug Plug.Parsers, parsers: [:json], json_decoder: Poison
 
   #
+  get "/api/webhook" do
+    # Respond the 200
+    conn |> resp(200, "")
+  end
+
+  #
   match "/api/webhook", via: [:post] do
     # Read the request body
     { :ok, body, conn } = read_body(conn)
@@ -23,11 +29,11 @@ defmodule Core.API do
     Core.Messenger.receive body
 
     # Respond the 200
-    send_resp(conn, 200, "")
+    conn |> resp(200, "OK")
   end
 
   # Catch ALL
   match _ do
-    send_resp(conn, 404, "Oops")
+    conn |> resp(404, "Oops")
   end
 end
