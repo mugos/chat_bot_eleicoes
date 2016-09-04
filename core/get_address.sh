eb42ad741eaad3c5add1efc2c781f478aecda0fe
yurifrl <yurifrl@hotmail.com>
2016-09-04 13:02:48 -0300

    bumpversion

--- core/get_address.sh
+++ core/get_address.sh
@@ -8,5 +8,7 @@ dg() {
     --volumes-from gcloud-config yurifl/gcloud $@
 }
 
+POD=$(dg kubectl get po | grep hackathon-core-web- | awk '{ print $1 }')
+
 docker-compose run --rm web curl -s http://ngrok:4040/api/tunnels | grep -o 'https\?:\/\/[a-zA-Z0-9\.]\+'
-dg kubectl exec -it hackathon-core-web-003d9c0ba6ff2c2a996d0e0c525f7bab-etki7 -- curl http://hackathon-core-ngrok:4040/api/tunnels | grep -o 'https\?:\/\/[a-zA-Z0-9\.]\+'
+dg kubectl exec -it $POD  -- curl http://hackathon-core-ngrok:4040/api/tunnels | grep -o 'https\?:\/\/[a-zA-Z0-9\.]\+'
