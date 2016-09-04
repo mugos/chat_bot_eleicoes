3c4146c3a5b54752d1c851ee0cd7db4e4f307ecb
yurifrl <yurifrl@hotmail.com>
2016-09-04 04:54:08 -0300

    ngrok hack

--- core/get_address.sh
+++ core/get_address.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker-compose run --rm web curl -s http://ngrok:4040/api/tunnels | grep -o 'https\?:\/\/[a-zA-Z0-9\.]\+'
+dg kubectl exec -it hackathon-core-web-6b9f3c753311b8f1f2b627718d2a2300-kugju -- curl http://hackathon-core-ngrok:4040/api/tunnels | grep -o 'https\?:\/\/[a-zA-Z0-9\.]\+'
